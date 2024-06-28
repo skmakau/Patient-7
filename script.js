5838a3a32df7276ab6696e00d84404fd7861b6a1
skmakau <103450393+skmakau@users.noreply.github.com>
2024-06-28 19:09:21 -0400

    Update script.js

--- script.js
+++ script.js
@@ -47,7 +47,7 @@ document.getElementById('surveyForm').addEventListener('submit', function(event)
         data[key] = value;
     });
 
-    fetch('https://script.google.com/macros/s/AKfycbwiW2pYISaB4oadWdHmlsiknT7QdyOqdLBYLkDlyqE9SMi4c2Ririzo6t2_qVGG3B7tEw/exec', {
+    fetch('https://script.google.com/macros/s/AKfycbzQXA0_q1cXff2APzkXia5lO4VA9gRflmozrDfw_JdADWuN61kEWntMvNnu2q9SAo7I/exec', {
         method: 'POST',
         headers: {
             'Content-Type': 'text/plain',
